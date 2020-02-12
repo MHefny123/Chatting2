9b6d8ddd0f64bd2c016cee258864c18033f5a7e7
MHefny <mhefnydev@gmail.com>
2020-02-12 13:28:44 +0200

    initial

---
+++ Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
+using System.IO;
+using System.Linq;
+
+namespace BlazorChatSample.Server
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddNewtonsoftJson();
+            services.AddSignalR();
+
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseResponseCompression();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBlazorDebugging();
+            }
+
+            app.UseStaticFiles();
+            // version 3.2.0 preview 1 - startup is now removed
+            app.UseClientSideBlazorFiles<Client.Program>();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+                // SignalR endpoint routing setup
+                endpoints.MapHub<Hubs.ChatHub>(Shared.ChatClient.HUBURL);
+
+                // version 3.2.0 preview 1 - startup is now removed
+                endpoints.MapFallbackToClientSideBlazor<Client.Program>("index.html");
+            });
+        }
+
+    }
+}
